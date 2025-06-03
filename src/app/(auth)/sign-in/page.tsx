f9b542c6215f2ec76c0de55feb30e10bd03f6551
Avinash-Tewari <avinashtewari03@gmail.com>
2025-06-03 16:47:39 +0530

    inc

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import { SignInview } from "@/auth/ui/views/sign-in-view";
+
+
+const Page = () =>{
+    return <SignInview/>
+            
+    
+}
+
+export default Page;
+
---
+++ src/auth/ui/views/sign-in-view.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { Card } from "@/components/ui/card";
+
+export const SignInview = () => {
+    console.log("Sign In View");
+    
+    return (
+        <Card>
+            sign in view
+        </Card>    
+    );
+};
+
